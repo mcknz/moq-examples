6ea28a1a2b1bd674d0c0e6afeaef5679abdbe43e
Jeff McKenzie <mail@mcknz.com>
2015-11-10 19:39:09 -0500

    added first test

---
+++ MoqExamples/MoqExamples.UnitTests/MoqExamples.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoqExamples.UnitTests
+{
+    [TestFixture]
+    public class MoqExamples
+    {
+        //_longRunningLibrary
+
+        [SetUp]
+        public void SetupForTest()
+        {
+           // _longRunningLibrary = new LongRunningLibrary();
+        }
+
+        [Test]
+        public void TestLongRunningLibrary()
+        {
+            const int interval = 10;
+            ILongRunningLibrary longRunningLibrary = new LongRunningLibrary();
+            var result = longRunningLibrary.RunForALongTime(interval);
+            Debug.WriteLine("Return from method was {0}", result);
+        }
+    }
+}
