55d0762a44f7753d094c61176f655713b6b3406c
Jeff McKenzie <mail@mcknz.com>
2015-11-10 19:47:01 -0500

    added interface and concrete class for library

---
+++ MoqExamples/MoqExamples.Library/LongRunningLibrary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MoqExamples.Library
+{
+    public class LongRunningLibrary :ILongRunningLibrary
+    {
+        public string RunForALongTime(int interval)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
