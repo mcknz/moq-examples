95e5b219e0ed9c1bf29a9fb4cbb17d74fbd344b4
Jeff McKenzie <mail@mcknz.com>
2015-11-12 01:24:34 -0500

    completed first ninject test

--- MoqExamples/MoqExamples.Ninject/BusinessService.cs
+++ MoqExamples/MoqExamples.Ninject/BusinessService.cs
@@ -8,20 +8,17 @@ namespace MoqExamples.Ninject
 {
     public class BusinessService
     {
-        private readonly string _databaseConnectionString = "dbString";
-        private readonly string _webServiceAddress = "webAddress";
-        private readonly ILoggingDataSink _loggingDataSink;
-
         private IDataAccessComponent _dataAccessComponent;
         private IWebServiceProxy _webServiceProxy;
         private ILoggingComponent _loggingComponent;
 
-        public BusinessService()
+        public BusinessService(ILoggingComponent loggingComponent,
+                               IWebServiceProxy webServiceProxy,
+                               IDataAccessComponent dataAccessComponent)
         {
-            _loggingDataSink = new LoggingDataSink();
-            _loggingComponent = new LoggingComponent(_loggingDataSink);
-            _webServiceProxy = new WebServiceProxy(_webServiceAddress);
-            _dataAccessComponent = new DataAccessComponent(_databaseConnectionString);
+            _loggingComponent = loggingComponent;
+            _webServiceProxy = webServiceProxy;
+            _dataAccessComponent = dataAccessComponent;
         }
 
     }
---
+++ MoqExamples/MoqExamples.UnitTests/NinjectTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Ninject;
+using MoqExamples.Ninject;
+
+namespace MoqExamples.UnitTests
+{
+    [TestFixture]
+    public class NinjectTests
+    {
+        [Test]
+        public void ShouldBeAbleToGetBusinessServiceFromNinject()
+        {
+            BusinessService actual;
+            var kernel = new StandardKernel(new CoreModule());
+            actual = kernel.Get<BusinessService>();
+            Assert.IsNotNull(actual);
+        }
+    }
+}
